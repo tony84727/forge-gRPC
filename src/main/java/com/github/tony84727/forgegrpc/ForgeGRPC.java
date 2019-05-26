866bb626138dc6803bb2bd389b62c16912a99bb2
tony84727 <tony84727@gmail.com>
2019-05-26 18:03:43 +0800

    add ServerController

--- src/main/java/com/github/tony84727/forgegrpc/ForgeGRPC.java
+++ src/main/java/com/github/tony84727/forgegrpc/ForgeGRPC.java
@@ -1,23 +1,12 @@
 package com.github.tony84727.forgegrpc;
 
-import com.github.tony84727.forgegrpc.service.Chat;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
+import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
 
 @Mod(modid=ForgeGRPC.MODID, version=ForgeGRPC.VERSION, name = ForgeGRPC.NAME)
 public class ForgeGRPC {
@@ -25,66 +14,22 @@ public class ForgeGRPC {
     public final static String VERSION = "0.0.0";
     public final static String NAME = "Forge GRPC";
     private static Logger logger;
-    private static Server server;
-    private static Side serverSide;
+    private static ServerController serverController;
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent e) {
     	logger = e.getModLog();
+    	serverController = new ServerController(logger);
+    	EVENT_BUS.register(serverController);
 	}
 
     @Mod.EventHandler
     public static void onServerStart(FMLServerStartingEvent e) {
-    	if (server != null) {
-    		server.shutdown();
-		}
-    	serverSide = Side.SERVER;
-    	final MinecraftServer mcServer = e.getServer();
-		server = getServerBuilder().addService(Chat.getInstance(logger, (message) -> {
-			final ITextComponent messageComponent = new TextComponentString(message);
-			mcServer.getPlayerList().getPlayers().forEach((player) ->
-				player.sendMessage(messageComponent)
-			);
-		})).build();
-		try {
-			server.start();
-			logger.info("gRPC enabled");
-		} catch (IOException exception) {
-			logger.error("unable to start gRPC server");
-			logger.error(exception);
-		}
+    	serverController.startServerSideGRPC(e.getServer());
     }
 
     @Mod.EventHandler
 	public static void onServerStopping(FMLServerStoppingEvent e) {
-    	if (server != null) {
-    		server.shutdown();
-    		server = null;
-		}
-		logger.info("grpc server shutdown");
-	}
-
-    @SideOnly(Side.CLIENT)
-    @Mod.EventHandler
-    public static void onConnectToServer(FMLNetworkEvent.ClientConnectedToServerEvent e) {
-    	if (server != null && serverSide != Side.SERVER) {
-    		server.shutdown();
-		}
-    	serverSide = Side.CLIENT;
-    	final EntityPlayer p = Minecraft.getMinecraft().player;
-    	server = getServerBuilder().addService(Chat.getInstance(logger, (message) ->
-			Minecraft.getMinecraft().player.sendChatMessage(message)
-		)).build();
-    	try {
-    		server.start();
-    		logger.info("gRPC enabled");
-		} catch (IOException exception) {
-			logger.error("unable to start gRPC server");
-    		logger.error(exception);
-		}
-    }
-
-    private static ServerBuilder getServerBuilder() {
-    	return ServerBuilder.forPort(30000);
+    	serverController.stopCurrentServer();
 	}
 }
---
+++ src/main/java/com/github/tony84727/forgegrpc/ServerController.java
@@ -0,0 +1,79 @@
+package com.github.tony84727.forgegrpc;
+
+import com.github.tony84727.forgegrpc.service.Chat;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+/*
+Manage lifecycle of grpc server by forge's event.
+ */
+public class ServerController
+{
+	private Server server;
+	private Side side;
+	private Logger logger;
+
+	public ServerController(Logger logger) {
+		this.logger = logger;
+	}
+
+	public void startServerSideGRPC(MinecraftServer mcServer) {
+		side = Side.SERVER;
+		server = getServerBuilder().addService(Chat.getInstance(logger, (message) -> {
+			final ITextComponent messageComponent = new TextComponentString(message);
+			mcServer.getPlayerList().getPlayers().forEach((player) ->
+				player.sendMessage(messageComponent)
+			);
+		})).build();
+		startServer();
+	}
+
+	public void stopCurrentServer() {
+		if (server == null) {
+			return;
+		}
+		server.shutdown();
+    	server = null;
+		logger.info("grpc server shutdown");
+	}
+
+	private void startServer() {
+		try {
+			server.start();
+			logger.info("gRPC enabled");
+		} catch (IOException exception) {
+			logger.error("unable to start gRPC server");
+			logger.error(exception);
+		}
+	}
+
+	private static ServerBuilder getServerBuilder() {
+    	return ServerBuilder.forPort(30000);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onClientConnectToServer(FMLNetworkEvent.ClientConnectedToServerEvent event) {
+		if (server != null && side != Side.SERVER) {
+    		server.shutdown();
+		}
+    	side = Side.CLIENT;
+    	final EntityPlayer p = Minecraft.getMinecraft().player;
+    	server = getServerBuilder().addService(Chat.getInstance(logger, (message) ->
+			Minecraft.getMinecraft().player.sendChatMessage(message)
+		)).build();
+    	startServer();
+	}
+}
